3481055f583d814807df7fd96738dbd508903bc0
AntonyCatcher <anton.chvertkov@yahoo.com>
2023-01-28 17:36:52 +0300

    Starting of Cooking Timer Project

--- Cooking Timer/ViewController.swift
+++ Cooking Timer/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let eggTimes = ["Soft"   : 5,
+                    "Medium" : 7,
+                    "Hard"   : 12]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.titleLabel?.text
+        
+        switch hardness {
+        case "Soft":
+            print(eggTimes.values)
+        case "Medium":
+            print(eggTimes.values)
+        case "Hard":
+            print(eggTimes.values)
+        default:
+            print("Error")
+        }
+        
+    }
+    
 }
 
