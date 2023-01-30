470e4c7cb0def25f2f0197f197d4a8f76efcda75
AntonyCatcher <anton.chvertkov@yahoo.com>
2023-01-30 13:19:28 +0300

    Finish the project

--- Cooking Timer/ViewController.swift
+++ Cooking Timer/ViewController.swift
@@ -6,34 +6,53 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft"   : 5,
-                    "Medium" : 7,
-                    "Hard"   : 12]
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
+    let eggTimes = ["Soft"   : 3,
+                    "Medium" : 4,
+                    "Hard"   : 7]
     
+    var totalTime = 0
+    var secondsPassed = 0
     
+    var timer = Timer()
+    var player: AVAudioPlayer!
+    
+    @IBOutlet var textLabel: UILabel!
+    @IBOutlet var progressView: UIProgressView!
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.titleLabel?.text
         
-        switch hardness {
-        case "Soft":
-            print(eggTimes.values)
-        case "Medium":
-            print(eggTimes.values)
-        case "Hard":
-            print(eggTimes.values)
-        default:
-            print("Error")
-        }
+        timer.invalidate()
+        guard let hardness = sender.titleLabel?.text else { return }
+        totalTime = eggTimes[hardness]!
         
+        progressView.progress = 0.0
+        secondsPassed = 0
+        textLabel.text = hardness
+
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
     }
     
+    @objc func timerAction() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressView.progress = Float(secondsPassed) / Float(totalTime)
+            
+           
+
+        } else {
+            timer.invalidate()
+            textLabel.text = "Done!"
+            playSound()
+        }
+    }
+    
+    func playSound() {
+       let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
 
